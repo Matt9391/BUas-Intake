cbd80305ebe90eb1f2cee9087472e1e96f9cbc3a
Matt9391 <matteonovelli2016@gmail.com>
2025-11-08 22:05:17 +0100

    Assets imported and adapted

--- Buas-Intake/game.cpp
+++ Buas-Intake/game.cpp
@@ -7,8 +7,14 @@ namespace Tmpl8
 	// -----------------------------------------------------------
 	// Initialize the application
 	// -----------------------------------------------------------
+
+	Sprite fish(new Surface("assets/2D/angryFish.tga"), 10);
+	int frame = 0;
+	int timer = 100;
+	int te = 0;
 	void Game::Init()
 	{
+		fish.SetFrame(0);
 	}
 	
 	// -----------------------------------------------------------
@@ -24,6 +30,14 @@ namespace Tmpl8
 	// -----------------------------------------------------------
 	void Game::Tick(float deltaTime)
 	{
-		
+		te += deltaTime;
+		if (te > timer) {
+			frame++;
+			te = 0;
+		}
+		frame = frame % 10;
+		fish.SetFrame(frame);
+		screen->Clear(0);
+		fish.Draw(screen, 10, 10);
 	}
 };
