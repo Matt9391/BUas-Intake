863b60045066634cb0af87bef1bf2a4a491de939
Jeremiah van Oosten <jeremiah@3dgep.com>
2026-03-12 22:31:59 +0100

    Convert ogg file to mp3.
    MP3 has better compression, with no noticeable loss in quality.

--- src/Utils/AudioManager.cpp
+++ src/Utils/AudioManager.cpp
@@ -1,69 +1,87 @@
 #include <thread>
 #include "AudioManager.h"
+#include <chrono>
 
 namespace Tmpl8 {
 
-	AudioManager::AudioManager() :
-	soundLoaded(false),
-	musicEnabled(true),
-	soundEffectsEnabled(true)
-	{}
+    AudioManager::AudioManager() :
+        soundLoaded(false),
+        musicEnabled(true),
+        soundEffectsEnabled(true)
+    {
+    }
 
-	void AudioManager::init() {
-		std::thread([this]() {
+    void AudioManager::init() {
+        //		std::thread([this]() {
 
-			this->backgroundMusic.loadSound("./assets/Audio/bgMusic.ogg");
-			//this->backgroundMusic.loadMusic("./assets/Audio/bgMusic.ogg");
-			this->coinSound.loadSound("./assets/Audio/coin.wav");
-			this->hurtSound.loadSound("./assets/Audio/hurt.wav");
-			this->tapSound.loadSound("./assets/Audio/tap.wav");
-	
-			this->backgroundMusic.setLooping(true);
+        auto t0 = std::chrono::high_resolution_clock::now();
+        this->backgroundMusic.loadMusic("assets/Audio/bgMusic.mp3");
+        auto t1 = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count();
+        printf("Background music loading took %lld ms\n", duration);
+        //this->backgroundMusic.loadMusic("assets/Audio/bgMusic.ogg");
+        this->coinSound.loadSound("assets/Audio/coin.wav");
+        auto t2 = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+        printf("Coin sound loading took %lld ms\n", duration);
+        this->hurtSound.loadSound("assets/Audio/hurt.wav");
+        auto t3 = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::milliseconds>(t3 - t2).count();
+        printf("hurt.wave took %lld ms to load\n", duration);
+        this->tapSound.loadSound("assets/Audio/tap.wav");
+        auto t4 = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::milliseconds>(t4 - t3).count();
+        printf("tap.wav took %lld ms to load\n", duration);
+        
+        this->backgroundMusic.setLooping(true);
 
-			this->soundLoaded = true;
+        this->soundLoaded = true;
 
-			this->setMusicPlaying(this->musicEnabled);
-		}).detach();
-	}
+        this->setMusicPlaying(this->musicEnabled);
+        auto t5 = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::milliseconds>(t5 - t0).count();
+        printf("Total audio loading took %lld ms\n", duration);
+        //	}).detach();
+    }
 
-	void AudioManager::setMusicPlaying(bool state){
-		this->musicEnabled = state;
-		
-		if (!this->soundLoaded)
-			return;
-		
-		if (state)
-			this->backgroundMusic.play();
-		else
-			this->backgroundMusic.stop();	
-	};
-	
-	void AudioManager::setSoundPlaying(bool state) {
-		this->soundEffectsEnabled = state;
-	};
+    void AudioManager::setMusicPlaying(bool state) {
+        this->musicEnabled = state;
 
-	void AudioManager::playCoinSound() {
-		if (!this->soundLoaded || !this->soundEffectsEnabled)
-			return;
+        if (!this->soundLoaded)
+            return;
 
-		this->coinSound.play();
-	};
+        if (state)
+            this->backgroundMusic.play();
+        else
+            this->backgroundMusic.stop();
+    };
 
-	void AudioManager::playHurtSound(){
-		if (!this->soundLoaded || !this->soundEffectsEnabled)
-			return;
+    void AudioManager::setSoundPlaying(bool state) {
+        this->soundEffectsEnabled = state;
+    };
 
-		this->hurtSound.play();
-	};
-	
-	void AudioManager::playTapSound() {
-		if (!this->soundLoaded || !this->soundEffectsEnabled)
-			return;
+    void AudioManager::playCoinSound() {
+        if (!this->soundLoaded || !this->soundEffectsEnabled)
+            return;
 
-		this->tapSound.play();
-	};
+        this->coinSound.play();
+    };
 
-	bool AudioManager::isLoaded() const {
-		return this->soundLoaded;
-	}
+    void AudioManager::playHurtSound() {
+        if (!this->soundLoaded || !this->soundEffectsEnabled)
+            return;
+
+        this->hurtSound.play();
+    };
+
+    void AudioManager::playTapSound() {
+        if (!this->soundLoaded || !this->soundEffectsEnabled)
+            return;
+
+        this->tapSound.play();
+    };
+
+    bool AudioManager::isLoaded() const {
+        return this->soundLoaded;
+    }
 }
